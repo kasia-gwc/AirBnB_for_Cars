18d5348c25850abfb85c6a9ba36b7f6ead9e3e68
Niels Rupf <nrupf@uchicago.edu>
2020-08-17 17:12:29 +0200

    more changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      redirect_to @user, notice: 'User was successfully edited.'
     else
       render :new
     end
