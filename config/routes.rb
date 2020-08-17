93d05cbb9d58b5795bd71a434bccc8c924f6cb83
Niels Rupf <nrupf@uchicago.edu>
2020-08-17 17:35:58 +0200

    QoL changes to controllers and logic - more to be done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'listings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[index edit show update]
+
+  resources :listings do
+    resources :user, only: :show
+  end
+  resources :bookings do
+    resources :user, only: :show
+  end
 end
