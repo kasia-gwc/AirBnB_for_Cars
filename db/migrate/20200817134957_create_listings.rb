82188bdf86ad9bbcee0edf9c38b38bbe45dac24c
Niels Rupf <nrupf@uchicago.edu>
2020-08-17 16:24:14 +0200

    fixed all recs on comments

--- db/migrate/20200817134957_create_listings.rb
+++ db/migrate/20200817134957_create_listings.rb
@@ -3,8 +3,9 @@ class CreateListings < ActiveRecord::Migration[6.0]
     create_table :listings do |t|
       t.string :vehicle_type
       t.string :name
-      t.string :availability
       t.string :location
+      t.string :start_date
+      t.string :end_date
 
       t.timestamps
     end
--- db/migrate/20200817135045_create_bookings.rb
+++ db/migrate/20200817135045_create_bookings.rb
@@ -3,7 +3,8 @@ class CreateBookings < ActiveRecord::Migration[6.0]
     create_table :bookings do |t|
       t.references :user, null: false, foreign_key: true
       t.references :listing, null: false, foreign_key: true
-      t.string :reservation
+      t.string :start_date
+      t.string :end_date
 
       t.timestamps
     end
