a853797a0254d1499f1e1deed1aa9c8df2ed26d6
Niels Rupf <nrupf@uchicago.edu>
2020-08-17 15:56:34 +0200

    added models and controllers, added index route to ListingsController

---
+++ db/migrate/20200817135045_create_bookings.rb
@@ -0,0 +1,11 @@
+class CreateBookings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :listing, null: false, foreign_key: true
+      t.string :reservation
+
+      t.timestamps
+    end
+  end
+end
