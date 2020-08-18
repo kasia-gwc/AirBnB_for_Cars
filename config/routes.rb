b921cc06aa78e4b73c3b4b60e13c93a2fcd40265
Niels Rupf <nrupf@uchicago.edu>
2020-08-18 11:36:54 +0200

    updated router to display correctt links

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :listings do
+    resources :booking, only: %i[new create]
     resources :user, only: :show
   end
-  resources :bookings do
+  resources :bookings, only: %i[index show edit update destroy] do
     resources :user, only: :show
   end
 end
