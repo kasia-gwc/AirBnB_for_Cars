89950b9e7a7a5676c38faebc461585721a946d7c
Jeroen Zwart <jeroen.zwart@live.com>
2020-08-18 12:42:16 +0200

    changed column namens listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_142213) do
+ActiveRecord::Schema.define(version: 2020_08_18_103315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,11 +29,15 @@ ActiveRecord::Schema.define(version: 2020_08_17_142213) do
   create_table "listings", force: :cascade do |t|
     t.string "vehicle_type"
     t.string "name"
-    t.string "location"
     t.string "start_date"
     t.string "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "street"
+    t.string "zip"
+    t.string "city"
+    t.string "country"
+    t.integer "price"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -51,6 +55,9 @@ ActiveRecord::Schema.define(version: 2020_08_17_142213) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "tel"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
