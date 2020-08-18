44a0547ad573a7bde74ad810bd1e448d0c051fbb
Jeroen Zwart <jeroen.zwart@live.com>
2020-08-18 16:51:36 +0200

    added seedlist and basic validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,7 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+  validates :vehicle_type, :name, :description, :street, :zip, :city, :country, :start_date, :end_date, presence: true
+  validates :name, uniqueness: { scope: :vehicle_type, message: "Name already exists" }
+  validates :description, length: { minimum: 30, too_short: "Please add a more detailed description" }
 end
