fbb6561c7849239ce50d883de2a1d89c2a068098
Jeroen Zwart <jeroen.zwart@live.com>
2020-08-18 16:58:17 +0200

    changes uniqueness

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,5 @@ class Listing < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
   validates :vehicle_type, :name, :description, :street, :zip, :city, :country, :start_date, :end_date, presence: true
-  validates :name, uniqueness: { scope: :vehicle_type, message: "Name already exists" }
   validates :description, length: { minimum: 30, too_short: "Please add a more detailed description" }
 end
