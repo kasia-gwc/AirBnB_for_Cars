ce30afdb794ef4c0c98e93ab17045ea97050b844
Niels Rupf <nrupf@uchicago.edu>
2020-08-18 10:38:37 +0200

    made bookingsController index/show/new actions and html files

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,29 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Bookings.find(params[:id])
+  end
+
+  def new
+    @booking = Bookings.new
+  end
+
+  def create
+    @booking = Bookings.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: 'Booking was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
