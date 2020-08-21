34b92dbfe3d1a0b5a99ac102893c0cbfc35d0c3f
Jeroen Zwart <jeroen.zwart@live.com>
2020-08-21 09:10:25 +0200

    made change to model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -12,13 +12,10 @@ class Listing < ApplicationRecord
 
   validates :vehicle_type, :name, :description, :street, :zip, :city, :country, :start_date, :end_date, :price, presence: true
   validates :description, length: { minimum: 15, too_short: "Please add a more detailed description" }
-  geocoded_by :street
-  geocoded_by :zip
-  geocoded_by :city
-  geocoded_by :country
-  after_validation :geocode, if: :will_save_change_to_address?
+  geocoded_by :address
+  after_validation :geocode
 
-  # def address
-  #  [street, zip, city, country].compact.join(', ')
-  # end
+  def address
+    [street, city, country].compact.join(', ')
+  end
 end
